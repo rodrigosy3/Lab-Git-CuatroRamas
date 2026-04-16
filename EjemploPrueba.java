608c2a4fc31b5879be38ce822a2a2699736f5dea
rodrigosy3 <sihues3@gmail.com>
2026-04-16 12:42:05 -0500

    Selección Final: V2 Recursiva consolidada en main

---
+++ EjemploPrueba.java
@@ -0,0 +1,5 @@
+public class EjemploPrueba {
+    public static void main(String[] args) {
+        System.out.println("Prueba V1 (Iterativa) de 5: " + Factorial.calcular(5));
+    }
+}
--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,8 @@
 // V2: Recursiva
 public class Factorial {
     public static long calcular(int n) {
-        if (n <= 1) return 1;
+        if (n <= 1)
+            return 1;
         return n * calcular(n - 1);
     }
-
+}
